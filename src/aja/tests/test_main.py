84a0b611c5dd24fd172e0b60cca2ef5f415742d0
Jukka Ojaniemi <jukka.ojaniemi+github@live.com>
2013-12-03 00:16:35 +0200

    Add tests.

---
+++ src/aja/tests/test_main.py
@@ -0,0 +1,34 @@
+import unittest
+# import docopt
+
+from aja.main import Aja
+
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        aja = Aja({'<name>': 'test'})
+        self.assertTrue(aja)
+        self.assertTrue(aja.config)
+
+    def test_actions(self):
+        aja = Aja({'<name>': 'test'})
+        actions = ['info', 'register', 'buildout', 'show-config',
+                   'deploy', 'clone', 'bootstrap', 'list', 'update']
+        self.assertEqual(actions, aja.actions.keys())
+        self.assertEqual(aja.actions['info'], aja.show_info)
+        self.assertEqual(aja.actions['register'], aja.register)
+        self.assertEqual(aja.actions['buildout'], aja.run_buildout)
+        self.assertEqual(aja.actions['show-config'], aja.show_config)
+        self.assertEqual(aja.actions['deploy'], aja.deploy)
+        self.assertEqual(aja.actions['clone'], aja.clone_buildout)
+        self.assertEqual(aja.actions['bootstrap'], aja.bootstrap_buildout)
+        self.assertEqual(aja.actions['list'], aja.list_buildouts)
+        self.assertEqual(aja.actions['update'], aja.update_buildout)
+
+
+if __name__ == '__main__':
+    unittest.main()
