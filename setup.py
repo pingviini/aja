186fc3b47253b1577aa9a3bc703e3ba2a4e6f8af
Asko Soukka <asko.soukka@iki.fi>
2014-05-31 07:13:27 +0300

    Upgrade dependencies; Add buildout example

--- setup.py
+++ setup.py
@@ -1,39 +1,31 @@
-import os
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
-
-setup(name='aja',
-      version='0.1.0',
-      description='Deploy buildouts with ease.',
-      long_description="%s\n%s" % (README, CHANGES),
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-      ],
-      author='Jukka Ojaniemi',
-      author_email='jukka.ojaniemi@gmail.com',
-      url='https://github.com/pingviini/aja',
-      keywords='deploy buildout',
-      package_dir={"": "src"},
-      packages=find_packages('src'),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'fabric',
-          'path.py',
-          'pycrypto==2.6.1',
-          'requests',
-          'zc.buildout',
-      ],
-      extras_require={
-          'docs': ['Sphinx']
-      },
-      test_suite='aja.tests',
-      entry_points={
-          'console_scripts': ['aja=aja.main:main']
-      },
-      )
+setup(
+    name='aja',
+    version='0.1.0',
+    description='Buildout-based deployment made safe and easy',
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGES.txt').read()),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    author='Jukka Ojaniemi',
+    author_email='jukka.ojaniemi@gmail.com',
+    url='https://github.com/pingviini/aja',
+    keywords='deploy buildout',
+    license='GPL',
+    package_dir={"": "src"},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'fabric',
+    ],
+    extras_require={
+        'docs': ['Sphinx']
+    },
+    test_suite='aja.tests',
+)
