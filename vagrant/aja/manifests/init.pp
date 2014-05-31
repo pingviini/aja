0bc6c4a5fb7916bcc097995e2a747a608e75008a
Asko Soukka <asko.soukka@iki.fi>
2014-05-31 07:13:03 +0300

    Simplify vagrant environment

--- vagrant/aja/manifests/init.pp
+++ vagrant/aja/manifests/init.pp
@@ -1,171 +1,132 @@
-class aja ($is_master = false, $master_private_key = "",
-           $master_public_key = ""){
+class aja (
+  $is_master = false,
+  $master_private_key = '',
+  $master_public_key = ''
+){
 
-  user { "aja":
-    ensure => "present",
-    home => "/home/aja",
+  group { 'buildout':
+    ensure => 'present'
+  }
+
+  user { 'buildout':
+    ensure => 'present',
+    home => '/home/buildout',
     managehome => true,
-    shell => "/bin/bash",
-    owner => "aja",
-    group => "aja",
+    shell => '/bin/bash',
+    gid => 'buildout',
+    require => Group['buildout']
   }
 
-  file { "/home/aja":
-    ensure => "directory",
+  file { '/home/buildout':
+    ensure => 'directory',
     mode => 0744,
-    owner => "aja",
-    group => "aja",
-    require => User["aja"],
+    owner => 'buildout',
+    group => 'buildout',
+    require => User['buildout']
   }
 
-  file { "/home/aja/.ssh":
-    ensure => "directory",
+  file { '/home/buildout/.ssh':
+    ensure => 'directory',
     mode => 0700,
-    owner => "aja",
-    group => "aja",
-    require => User["aja"],
+    owner => 'buildout',
+    group => 'buildout',
+    require => User['buildout']
   }
 
-  file { "/home/aja/.buildout":
-    ensure => "directory",
-    owner => "aja",
-    group => "aja",
-    require => User["aja"],
+  file { '/home/buildout/.buildout':
+    ensure => 'directory',
+    owner => 'buildout',
+    group => 'buildout',
+    require => User['buildout']
   }
 
-  file { "/var/buildout":
-    ensure => "directory",
-    owner => "aja",
-    group => "aja",
-    require => User["aja"],
+  file { '/var/buildout':
+    ensure => 'directory',
+    owner => 'buildout',
+    group => 'buildout',
+    require => User['buildout']
   }
 
-  file { "/var/buildout/eggs-directory":
-    ensure => "directory",
-    owner => "aja",
-    group => "aja",
-    require => File["/var/buildout"],
+  file { '/var/buildout/eggs-directory':
+    ensure => 'directory',
+    owner => 'buildout',
+    group => 'buildout',
+    require => File['/var/buildout']
   }
 
-  exec { "chown -R aja /var/buildout/eggs-directory":
-    require => File["/var/buildout/eggs-directory"],
-    path => ["/bin"]
+  exec { 'chown -R buildout /var/buildout/eggs-directory':
+    require => File['/var/buildout/eggs-directory'],
+    path => ['/bin']
   }
 
-  file { "/var/buildout/download-cache":
-    ensure => "directory",
-    owner => "aja",
-    group => "aja",
-    require => File["/var/buildout"],
+  file { '/var/buildout/download-cache':
+    ensure => 'directory',
+    owner => 'buildout',
+    group => 'buildout',
+    require => File['/var/buildout']
   }
 
-  exec { "chown -R aja /var/buildout/download-cache":
-    require => File["/var/buildout/download-cache"],
-    path => ["/bin"],
+  exec { 'chown -R buildout /var/buildout/download-cache':
+    require => File['/var/buildout/download-cache'],
+    path => ['/bin']
   }
 
-  file { "/var/buildout/extends-cache":
-    ensure => "directory",
-    owner => "aja",
-    group => "aja",
-    require => File["/var/buildout"],
+  file { '/var/buildout/extends-cache':
+    ensure => 'directory',
+    owner => 'buildout',
+    group => 'buildout',
+    require => File['/var/buildout']
   }
 
-  exec { "chown -R aja /var/buildout/extends-cache":
-    require => File["/var/buildout/extends-cache"],
-    path => ["/bin"]
+  exec { 'chown -R buildout /var/buildout/extends-cache':
+    require => File['/var/buildout/extends-cache'],
+    path => ['/bin']
   }
 
-  file { "/home/aja/.buildout/default.cfg":
-    ensure => "present",
-    content => "[buildout]
+  file { '/home/buildout/.buildout/default.cfg':
+    ensure => 'present',
+    content => '[buildout]
 eggs-directory = /var/buildout/eggs-directory
 download-cache = /var/buildout/download-cache
-extends-cache = /var/buildout/extends-cache",
-    owner => "aja",
-    group => "aja",
+extends-cache = /var/buildout/extends-cache',
+    owner => 'buildout',
+    group => 'buildout',
     require => [
-    File["/var/buildout/eggs-directory"],
-    File["/var/buildout/download-cache"],
-    File["/var/buildout/extends-cache"]
+      File['/var/buildout/eggs-directory'],
+      File['/var/buildout/download-cache'],
+      File['/var/buildout/extends-cache']
     ]
   }
 
   if $is_master {
-    vcsrepo { "/home/aja/aja":
-      ensure => present,
-      provider => git,
-      source => "https://github.com/pingviini/aja",
-      user => "aja",
-      require => User["aja"],
-    }
-
-    exec { "python /home/aja/aja/setup.py install":
-      cwd => "/home/aja/aja",
-      user => "root",
-      path => ["/bin", "/usr/bin"],
-      require => Vcsrepo["/home/aja/aja"]
-    }
-
-    file { "/home/aja/.aja":
-      ensure => "directory",
-      owner => "aja",
-      group => "aja",
-      require => User["aja"],
-    }
-
-    file { "/home/aja/.aja/sites":
-      ensure => "directory",
-      owner => "aja",
-      group => "aja",
-      require => File["/home/aja/.aja"]
-    }
-
-    file { "/home/aja/.aja/config.cfg":
-      ensure => "present",
-      owner => "aja",
-      group => "aja",
-      require => File["/home/aja/.aja"],
-      content => '[global]
-buildouts-folder = /var/buildout
-buildouts-config-folder = /home/aja/.aja/sites
-effective-user = aja
-buildout-user = aja
-
-[git]
-path = /usr/bin/git
-
-[hg]
-path = /usr/bin/hg'}
-
     if $master_private_key {
-      file { "/home/aja/.ssh/id_rsa.pub":
-        ensure => "present",
+      file { '/home/buildout/.ssh/id_rsa.pub':
+        ensure => 'present',
         content => $master_public_key,
         mode => 0644,
-        require => File["/home/aja/.ssh"],
-        owner => "aja",
-        group => "aja",
+        require => File['/home/buildout/.ssh'],
+        owner => 'buildout',
+        group => 'buildout'
       }
-      file { "/home/aja/.ssh/id_rsa":
-        ensure => "present",
+      file { '/home/buildout/.ssh/id_rsa':
+        ensure => 'present',
         content => $master_private_key,
         mode => 0600,
-        require => File["/home/aja/.ssh"],
-        owner => "aja",
-        group => "aja",
+        require => File['/home/buildout/.ssh'],
+        owner => 'buildout',
+        group => 'buildout'
       }
     }
   }
   else {
     if $master_public_key {
-      file { "/home/aja/.ssh/authorized_keys":
-        ensure => "present",
+      file { '/home/buildout/.ssh/authorized_keys':
+        ensure => 'present',
         mode => 0600,
         content => $master_public_key,
-        owner => "aja",
-        group => "aja",
-        require => File["/home/aja/.ssh"]
+        owner => 'buildout',
+        group => 'buildout',
+        require => File['/home/buildout/.ssh']
       }
     }
   }
--- vagrant/staging.pp
+++ vagrant/staging.pp
@@ -1,87 +1,82 @@
-class { "epel": }
-
+class { 'epel': }
 class bootstrap {
   package {[
-    "autoconf",
-    "automake",
-    "asciidoc",
-    "binutils",
-    "bison",
-    "byacc",
-    "bzip2-devel",
-    "cscope",
-    "ctags",
-    "cvs",
-    "db4-devel",
-    "diffstat",
-    "doxygen",
-    "elfutils",
-    "elinks",
-    "expat-devel",
-    "flex",
-    "freetype-devel",
-    "gcc",
-    "gcc-c++",
-    "gcc-gfortran",
-    "gdbm-devel",
-    "gettext",
-    "git",
-    "htop",
-    "indent",
-    "intltool",
-    "iptraf",
-    "kernel-devel",
-    "libcurl-devel",
-    "libjpeg-turbo-devel",
-    "libpng-devel",
-    "libtool",
-    "libxslt-devel",
-    "lynx",
-    "make",
-    "openldap-clients",
-    "openldap-devel",
-    "openssl-devel",
-    "patch",
-    "patchutils",
-    "pkgconfig",
-    "python",
-    "python-devel",
-    "python-virtualenv",
-    "python-pip",
-    "rcs",
-    "readline-devel",
-    "subversion",
-    "swig",
-    "systemtap",
-    "unzip",
-    "vim-enhanced",
-    "wget",
-    "zip",
-    "zlib-devel"]:
-  ensure => "installed",
-  require => Class["epel"],
+    'autoconf',
+    'automake',
+    'asciidoc',
+    'binutils',
+    'bison',
+    'byacc',
+    'bzip2-devel',
+    'cscope',
+    'ctags',
+    'cvs',
+    'db4-devel',
+    'diffstat',
+    'doxygen',
+    'elfutils',
+    'elinks',
+    'expat-devel',
+    'flex',
+    'freetype-devel',
+    'gcc',
+    'gcc-c++',
+    'gcc-gfortran',
+    'gdbm-devel',
+    'gettext',
+    'git',
+    'htop',
+    'indent',
+    'intltool',
+    'iptraf',
+    'kernel-devel',
+    'libcurl-devel',
+    'libjpeg-turbo-devel',
+    'libpng-devel',
+    'libtool',
+    'libxslt-devel',
+    'lynx',
+    'make',
+    'openldap-clients',
+    'openldap-devel',
+    'openssl-devel',
+    'patch',
+    'patchutils',
+    'pkgconfig',
+    'python',
+    'python-devel',
+    'python-virtualenv',
+    'python-pip',
+    'rcs',
+    'readline-devel',
+    'subversion',
+    'swig',
+    'systemtap',
+    'unzip',
+    'vim-enhanced',
+    'wget',
+    'zip',
+    'zlib-devel']:
+    ensure => 'installed',
+    require => Class['epel'],
   }
-
-  package { ["mercurial_keyring", "mercurial"]:
-    provider => "pip",
-    ensure   => "installed"
+  package {['mercurial_keyring', 'mercurial']:
+    provider => 'pip',
+    ensure => 'installed',
+    require => Package['python-pip'],
   }
 }
-
-
-class { "bootstrap": }
-class { "aja":
+class { 'bootstrap': }
+class { 'aja':
   is_master => true,
   master_private_key => $master_private_key,
-  master_public_key => $master_public_key
+  master_public_key => $master_public_key,
+  require => Class['bootstrap'],
 }
-
-file { "/etc/hosts":
-  ensure => "present",
+file { '/etc/hosts':
+  ensure => 'present',
   content => "127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1 localhost localhost.localdomain localhost6 localhost6.localdomain6
-192.168.50.11 production
-192.168.50.12 development
+${production} production
 "
 }
-
