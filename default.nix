5cfbebb0c12ad7fdf9b10f32fe870012fb6c4f36
Asko Soukka <asko.soukka@iki.fi>
2016-04-26 14:41:00 +0300

    Update development environment

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import (builtins.fetchTarball  # revision for reproducible builds
+  "https://github.com/nixos/nixpkgs-channels/archive/nixos-16.03.tar.gz") {}
+, pythonPackages ? pkgs.python27Packages
+}:
+
+let self = rec {
+  buildout = pythonPackages.zc_buildout_nix.overrideDerivation (args: {
+    postInstall = "";
+    propagatedNativeBuildInputs = [
+      pythonPackages.paramiko
+    ];
+  });
+};
+in pkgs.stdenv.mkDerivation rec {
+  name = "env";
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+  builder = builtins.toFile "builder.sh" ''
+    source $stdenv/setup; ln -s $env $out
+  '';
+  buildInputs = with self; [
+    buildout
+    (pkgs.pythonFull.buildEnv.override {
+      extraLibs = buildout.propagatedNativeBuildInputs;
+    })
+  ];
+  shellHook = ''
+    export BUILDOUT_ARGS="\
+        versions:setuptools= \
+        versions:zc.buildout="
+  '';
+}
